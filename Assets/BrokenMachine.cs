76644773564296e9335579a20ee5b25054107ab7
Joel Barr <Joelbarr1220@gmail.com>
2024-08-10 22:07:35 -0600

    First bot can be repaired!

--- Assets/BrokenMachine.cs
+++ Assets/BrokenMachine.cs
@@ -1,9 +1,38 @@
 using MoreMountains.CorgiEngine;
+using MoreMountains.InventoryEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BrokenMachine : ButtonActivated
 {
+	public BaseItem PartNeeded;
 
+    private const int PARTS_REQUIRED = 3;
+
+    public override void TriggerButtonAction(GameObject instigator)
+	{
+		if (!CheckNumberOfUses()) {
+			return;
+		}
+
+		Collider2D coll = instigator.GetComponent<Collider2D>();
+		if (coll != null) {
+			base.TriggerButtonAction(instigator);
+			
+			// Retrieve parts from inventory
+			var inv = instigator.GetComponent<CharacterInventory>().MainInventory;
+			
+			// Check if we have enough parts (Player inventory)
+			int partsCount = inv.GetQuantity(PartNeeded.ItemID);
+			if (partsCount >= PARTS_REQUIRED) {
+				// TODO: Fix this machine
+				Debug.Log($"Fix!");
+				GetComponent<Animator>().SetTrigger("fixed");
+			} else {
+				// TODO: Say something about not having enough parts
+				Debug.Log($"Not enough pieces! ({partsCount}/{PARTS_REQUIRED})");
+			}
+		}
+	}
 }
